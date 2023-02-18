750c8a1bc9467303c581a754ce41bb90ef792e01
ferebel <rebelatofelipe@gmail.com>
2023-02-18 22:41:34 +0000

    add some array types and variables

---
+++ main.js
@@ -0,0 +1,42 @@
+let firstName = 'Felipe'
+firstName = 'Felipe';
+
+const PI = 3.14
+
+//let arr = Array()
+//creating array with constructor Array
+
+
+// const arr = []
+// console.log(arr)
+// syntax
+// This the most recommended way to create an empty list
+
+
+const numbers = [0, 3.14, 9.81, 98.6, 100] //array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] //array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] //array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] //array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB'] //array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] //array of strings, countries
+
+//print the array and it's length
+
+// console.log('Numbers:', numbers)
+// console.log('Number of numbers:', numbers.length)
+
+// console.log('Fruits:', fruits)
+// console.log('Number of Fruits:', fruits.length)
+
+// console.log('Vegetables:', vegetables)
+// console.log('Number of vegetables:', vegetables.length)
+
+// console.log('Animal products:', animalProducts)
+// console.log('Number of animal products:', animalProducts.length)
+
+// console.log('Web technologies:', webTechs)
+// console.log('Number of Web Technologies:', webTechs.length)
+
+// console.log('Countries:', countries)
+// console.log('Number of Countries:', countries.length)
+
